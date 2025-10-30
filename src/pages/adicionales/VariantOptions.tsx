f7f0d2c258772aecedefb55f92350b803497ba1e
Guillermo Ramirez Tenorio <40372863+MemoRT22@users.noreply.github.com>
2025-10-30 13:46:04 -0600

    Added VariantOptions.tsx

---
+++ src/pages/adicionales/VariantOptions.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import UnderConstruction from '../../components/UnderConstruction';
+
+export default function VariantOptions() {
+  return <UnderConstruction pageName="Variantes" />;
+}
